214f7402a8b6e8d303033fe2f90170c827f9d5ec
shivam2575 <sk2575@gmail.com>
2024-12-16 16:36:07 +0530

    EP-3 part 1

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const nestedEle = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "head1" }, "This is heading of child 1"),
-    React.createElement("p", { id: "para1" }, "This is paragraph of child 1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "head2" }, "This is heading of child 2"),
-    React.createElement("p", { id: "para2" }, "This is paragraph of child 2"),
-  ]),
-]);
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World Using React"
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("div", { id: "heading" }, "Namaste React");
+
+// JSX components
+const jsxHeading = (
+  <h1 id="jsxHeading" tabIndex="5">
+    Namaste React using JSX
+  </h1>
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(nestedEle);
+// rendering react objects using react
+root.render(heading);
